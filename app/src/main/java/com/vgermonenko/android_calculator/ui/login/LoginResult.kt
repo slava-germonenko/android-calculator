d54ff6be01d5c2a28eb40c2b559f466bc8691e03
Viacheslav Germonenko <vi.germonenko@outlook.com>
2020-03-23 19:38:34 +0300

    Added new activity.

---
+++ app/src/main/java/com/vgermonenko/android_calculator/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.vgermonenko.android_calculator.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
